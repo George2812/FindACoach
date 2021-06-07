3f9917bea750dfe8500768944b21ce2a9c0d3beb
George Dialynas <dialynas1@yahoo.com>
2021-06-07 15:39:18 +0300

    added firebase server - implemented Http Request methods

--- src/pages/coaches/CoachesList.vue
+++ src/pages/coaches/CoachesList.vue
@@ -1,18 +1,31 @@
 <template>
+  <base-dialog
+    :show="!!error"
+    title="An error occured!"
+    @close="handleError"
+  >
+    <p>{{ error }}</p>
+  </base-dialog>
   <section>
     <coach-filter @change-filter="setFilters"></coach-filter>
   </section>
   <section>
     <base-card>
       <div class="controls">
-        <base-button mode="outline">Refresh</base-button>
         <base-button
-          v-if="!isCoach"
+          mode="outline"
+          @click="loadCoaches"
+        >Refresh</base-button>
+        <base-button
+          v-if="!isCoach && !isLoading"
           link
           to="/register"
         >Register as a Coach</base-button>
       </div>
-      <ul v-if="hasCoaches">
+      <div v-if="isLoading">
+        <base-spinner></base-spinner>
+      </div>
+      <ul v-else-if="hasCoaches">
         <coach-item
           v-for="coach in filteredCoaches"
           :key="coach.id"
@@ -36,6 +49,8 @@ export default {
   components: { CoachItem, CoachFilter },
   data() {
     return {
+      isLoading: false,
+      error: null,
       activeFilters: {
         frontend: true,
         backend: true,
@@ -65,12 +80,28 @@ export default {
 
     },
     hasCoaches() {
-      return this.$store.getters['coaches/hasCoaches'];
+      return !this.isLoading && this.$store.getters['coaches/hasCoaches'];
     }
+  },
+  created() {
+    this.loadCoaches();
   },
   methods: {
     setFilters(updatedFilters) {
       this.activeFilters = updatedFilters;
+    },
+    async loadCoaches() {
+      this.isLoading = true;
+      try {
+        await this.$store.dispatch('coaches/loadCoaches');
+      } catch (error) {
+        this.error = error.message || 'Something went wrong';
+      }
+
+      this.isLoading = false;
+    },
+    handleError() {
+      this.error = null;
     }
   }
 }
