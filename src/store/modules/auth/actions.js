f352e4451875d7ba1af7c9ee0afbb46cd61440de
George Dialynas <dialynas1@yahoo.com>
2021-06-10 12:41:28 +0300

    added auto login and logout

--- src/store/modules/auth/actions.js
+++ src/store/modules/auth/actions.js
@@ -1,30 +1,27 @@
+let timer;
+
 export default {
     async login(context, payload) {
-        const response = await fetch('https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyBBlFuQ4HJJc1pnc6mgpGnOnxhWkUm7E5U', {
-            method: 'POST',
-            body: JSON.stringify({
-                email: payload.email,
-                password: payload.password,
-                returnSecureToken: true //this is inside firebase REST API google
-            })
-        });
-        const responseData = await response.json();
-
-        if (!response.ok) {
-            console.log(responseData);
-            const error = new Error(responseData.message || 'Failed to authenticate. Check your login data.');
-            throw error;
-        }
-
-        console.log(responseData);
-        context.commit('setUser', {
-            token: responseData.idToken,
-            userId: responseData.localId,
-            tokenExpiration: responseData.expiresIn
+        return context.dispatch('auth', {
+            ...payload,
+            mode: 'login'
         })
     },
     async signup(context, payload) {
-        const response = await fetch('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBBlFuQ4HJJc1pnc6mgpGnOnxhWkUm7E5U', {
+        return context.dispatch('auth', {
+            ...payload,
+            mode: 'signup'
+        })
+    },
+    async auth(context, payload) {
+        const mode = payload.mode;
+        let url = 'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyBBlFuQ4HJJc1pnc6mgpGnOnxhWkUm7E5U';
+
+        if (mode === 'signup') {
+            url = 'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBBlFuQ4HJJc1pnc6mgpGnOnxhWkUm7E5U';
+        }
+
+        const response = await fetch(url, {
             method: 'POST',
             body: JSON.stringify({
                 email: payload.email,
@@ -35,23 +32,63 @@ export default {
         const responseData = await response.json();
 
         if (!response.ok) {
-            console.log(responseData);
             const error = new Error(responseData.message || 'Failed to authenticate. Check your login data.');
             throw error;
         }
 
-        console.log(responseData);
+        const expiresIn = +responseData.expiresIn * 1000;
+        const expirationDate = new Date().getTime() + expiresIn;
+
+
+        localStorage.setItem('token', responseData.idToken);
+        localStorage.setItem('userId', responseData.localId);
+        localStorage.setItem('tokenExpiration', expirationDate);
+
+        timer = setTimeout(function () {
+            context.dispatch('autoLogout');
+        }, expiresIn);
+
         context.commit('setUser', {
             token: responseData.idToken,
             userId: responseData.localId,
-            tokenExpiration: responseData.expiresIn
         })
     },
+    tryLogin(context) {
+        const token = localStorage.getItem('token');
+        const userId = localStorage.getItem('userId');
+        const tokenExpiration = localStorage.getItem('tokenExpiration');
+
+        const expiresIn = +tokenExpiration - new Date().getTime();
+
+        if (expiresIn < 0) {
+            return;
+        }
+
+        setTimeout(function () {
+            context.dispatch('autoLogout');
+        }, expiresIn);
+
+        if (token && userId) {
+            context.commit('setUser', {
+                token: token,
+                userId: userId,
+            })
+        }
+    },
     logout(context) {
+        localStorage.removeItem('token');
+        localStorage.removeItem('userId');
+        localStorage.removeItem('tokenExpiraton');
+
+        clearTimeout(timer);
+
         context.commit('setUser', {
             token: null,
             userId: null,
-            tokenExpiration: null
         })
+    },
+    autoLogout(context) {
+        context.dispatch('logout');
+        context.commit('setAutoLogout');
     }
 };
